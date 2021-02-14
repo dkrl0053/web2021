5049bb216c7f52f693feef20a351c1831eb270d7
dkrl0053 <75105340+dkrl0053@users.noreply.github.com>
2021-02-14 23:19:35 +0900

    updata

--- 04company/js/headerEvent.js
+++ 04company/js/headerEvent.js
@@ -13,7 +13,49 @@ header_wrap.addEventListener('mouseout', function(){
   header_wrap.classList.remove('on');
 })
 
-// header_scroll.addEventListener('wheel')
+
+
+
+
+
+
+$(window).scroll(function(){
+  const scrollY = $(window).scrollTop();
+  const windowY = window.scrollY;
+
+  if(windowY > 500){
+    $(".top").addClass("view");
+  } else if(windowY <= 500){
+    $(".top").removeClass("view");
+  }
+
+
+  console.log(windowY);
+  if(windowY > 300){
+    $(".header_wrap").addClass("scroll_menu");
+  } else if(windowY <= 300){
+    $(".header_wrap").removeClass("scroll_menu");
+  }
+});
+
+$(".top").click(function(){
+  $("html,body").animate({scrollTop:0},1000);
+});
+
+
+$(".toggle_menu").click(function(){
+  const toggle = $(".toggle_menu").attr("class");
+  if(toggle == "toggle_menu"){
+      $(".toggle_menu").addClass("change");
+      $(".header_gnb").stop().animate({left:0},300);
+  }else{
+      $(".toggle_menu").removeClass("change");
+      $(".header_gnb").stop().animate({left:"-100%"},300);
+  }
+});
+
+
+
 // mobile
 // const toggleBtn = document.querySelector('.toggle_menu');
 
